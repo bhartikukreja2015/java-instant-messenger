9cf2de77ed572fb08c563368b44099425fa5744e
ryan <ryan@rmarcus.info>
2009-01-23 19:29:41 +0000

    Starting rendering stuff

---
+++ guiStuff/BuddyRenderer.java
@@ -0,0 +1,16 @@
+package guiStuff;
+
+import java.awt.Canvas;
+import java.awt.Component;
+
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class BuddyRenderer extends Canvas implements ListCellRenderer {
+
+	public Component getListCellRendererComponent(JList arg0, Object arg1, int arg2, boolean arg3, boolean arg4) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
