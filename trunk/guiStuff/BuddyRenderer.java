123d029a09e0f9fa6bf25257d38d67bd1e2b72c7
ryan@rmarcus.info <ryan@rmarcus.info@9b9088b4-e7de-11dd-a971-7dbc132099af>
2009-01-24 03:23:03 +0000

    Fix

--- trunk/guiStuff/BuddyRenderer.java
+++ trunk/guiStuff/BuddyRenderer.java
@@ -1,16 +1,42 @@
 package guiStuff;
 
-import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Graphics;
 
-import javax.swing.JList;
-import javax.swing.ListCellRenderer;
+import abstractionLayer.Buddy;
 
-public class BuddyRenderer extends Canvas implements ListCellRenderer {
+public class BuddyRenderer extends Component {
 
-	public Component getListCellRendererComponent(JList arg0, Object arg1, int arg2, boolean arg3, boolean arg4) {
-		// TODO Auto-generated method stub
-		return null;
+	private static final long serialVersionUID = 1L;
+
+	Buddy toShow; 
+	
+	public BuddyRenderer(Buddy theBuddy) {
+		super();
+		toShow = theBuddy;
 	}
-
+	
+	public void paint(Graphics g) {
+		// for now, let's just print the name of the user in a certain color
+		// green for av.
+		// red for away
+		// gray for offline
+		
+		int yBaseline = (g.getClipBounds().height / 2) - (g.getFont().getSize() / 2);
+		
+		// drawString renders the text by baseline, so we want to position the text in the middle
+		// so we go half way down, then up half the font size.
+		
+		if (toShow.getStatus() == Buddy.away || toShow.getStatus() == Buddy.DoNotDistrub || toShow.getStatus() == Buddy.superAway) {
+			g.setColor(new Color(255,0,0));
+		} else if (toShow.getStatus() == Buddy.available || toShow.getStatus() == Buddy.superAvailable) {
+			g.setColor(new Color(0,255,0));
+		} else {
+			g.setColor(new Color(200,200,200));
+		}
+		
+		g.drawString(toShow.getScreename(), 10, yBaseline);
+		
+	}
 }
