a4c1609f672a350443972aab131d33233c5845d1
ryan <ryan@rmarcus.info>
2009-01-28 19:25:59 +0000

    Started some rendering stuff

--- guiStuff/IMWindowJLabel.java
+++ guiStuff/IMWindowJLabel.java
@@ -1,9 +1,58 @@
 package guiStuff;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
 import javax.swing.JLabel;
+
+import abstractionLayer.Buddy;
+
+import mainIconSet.IconFetch;
 
 public class IMWindowJLabel extends JLabel {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	
+	private static final int graphicX = 5;
+	private static final int graphicY = 5;
+	protected static final int graphicTextSpace = 10;
+	
+	protected Buddy toRender;
+	
+	public IMWindowJLabel()  {
+		
+	}
+	
+	public void setBuddy(Buddy b) {
+		toRender = b;
+	}
+	
+	public void paint(Graphics g) {
+		int theHeight = this.getHeight();
+		int theWidth = this.getWidth();
+		
+		IconFetch theFetch = new IconFetch();
+		
+		Image theImg = theFetch.loadImage(toRender.getStatus());
+		
+		int graphicHeight = theImg.getHeight(null);
+		int graphicWidth = theImg.getWidth(null);
+		
+		
+		
+		double scalingFactor = theHeight - (IMWindowJLabel.graphicY * 2); // inital Y we have
+		scalingFactor = graphicHeight / scalingFactor;
+		
+		int iSF = (int) scalingFactor;
+		// scalingFactor is now orignal / render
+		
+		g.drawImage(theFetch.loadImage(toRender.getStatus()), graphicX, graphicY, graphicHeight * iSF, graphicWidth * iSF, null);
+	}
+	
+	
+	public Dimension getPreferredSize() {
+		return new Dimension(0, 32);
+	}
 }
