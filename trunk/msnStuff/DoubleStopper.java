7b3e928ca8d2a1276b416a0dab64f389e27271ef
ryan@rmarcus.info <ryan@rmarcus.info@9b9088b4-e7de-11dd-a971-7dbc132099af>
2009-04-08 02:25:04 +0000

    Fixed MSN multiple contact updates bug

---
+++ trunk/msnStuff/DoubleStopper.java
@@ -0,0 +1,28 @@
+package msnStuff;
+
+import java.util.ArrayList;
+
+public class DoubleStopper {
+	protected ArrayList<String> theBuddies;
+	
+	
+	public DoubleStopper() {
+		theBuddies = new ArrayList<String>();
+	}
+	
+	public boolean checkBuddy(String username) {
+		int i = 0;
+		while (i != theBuddies.size()) {
+			if (theBuddies.get(i).equals(username)) { return false; }
+			i++;
+		}
+		
+		theBuddies.add(username);
+		
+		return true;
+	}
+	
+	public void reset() {
+		theBuddies = new ArrayList<String>();
+	}
+}
