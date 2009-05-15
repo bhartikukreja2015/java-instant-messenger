f4dc943edae521ab874b9364acdfad6aef55ec0f
ryan <ryan@rmarcus.info>
2009-05-15 01:39:21 +0000

    More cleaning, more dew.

--- guiStuff/dewStuff/AccountAction.java
+++ guiStuff/dewStuff/AccountAction.java
@@ -2,28 +2,60 @@ package guiStuff.dewStuff;
 
 import java.awt.Image;
 
+import jimPreferences.PreferencePoint;
+
+import mainIconSet.IconFetch;
+
+import upperAbstractionLayer.AccountManager;
+
+import abstractionLayer.AbstractAccount;
+import abstractionLayer.AccountSettings;
+
 public class AccountAction implements Action {
 
-	public AccountAction(AccountSettings)
+	protected AccountSettings theSettings;
+	protected AccountManager AM;
+	
+	public AccountAction(AccountSettings as, AccountManager theAM) { 
+		theSettings = as;
+		AM = theAM;
+	}
 	
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		if (isConnected()) {
+			AM.disconnectAccount(theSettings);
+		} else {
+			AM.connectWithTemplate(theSettings);
+		}
 	}
 
 	public String getDesc() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isConnected()) {
+			return "Disconnect the account " + theSettings.getAlias();
+		} else {
+			return "Connect with the account " + theSettings.getAlias();
+		}
 	}
 
 	public Image getImage() {
-		// TODO Auto-generated method stub
-		return null;
+		PreferencePoint pp = new PreferencePoint();
+		IconFetch IF = new IconFetch(pp.getIconTheme());
+		
+		return IF.loadImage(theSettings.getAccountType());
 	}
 
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isConnected()) {
+			return "Disconnect " + theSettings.getAlias();
+		} else {
+			return "Connect with " + theSettings.getAlias();
+		}
+	}
+	
+	protected boolean isConnected() {
+		// see if we are connected...
+		AbstractAccount as = AM.getAccount(theSettings);
+		return as.isConnected();
 	}
 
 }
